05ca0e0c9db48318af945c85dd1b45ad1159c945
MHS195 <79396663+MHS195@users.noreply.github.com>
2026-01-24 09:17:36 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,12 @@ if [ "$#" -ne 3 ]; then
     echo "Usage: $0 vendor codename architecture"
     exit 1
 fi
-
+#
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install -y bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick protobuf-compiler python3-protobuf lib32readline-dev lib32z1-dev libdw-dev libelf-dev libgnutls28-dev lz4 libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev python-is-python3 mkbootimg cpio
+wget https://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.3-2_amd64.deb && sudo dpkg -i libtinfo5_6.3-2_amd64.deb && rm -f libtinfo5_6.3-2_amd64.deb && wget https://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.3-2_amd64.deb && sudo dpkg -i libncurses5_6.3-2_amd64.deb && rm -f libncurses5_6.3-2_amd64.deb
+#
 # Create Lindroid and TheMuppets manifests
 #rm -rf .repo/local_manifests
 #repo init -u https://github.com/LineageOS-UL/android.git -b lineage-21.0 --git-lfs
